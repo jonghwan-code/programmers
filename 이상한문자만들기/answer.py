34bdcfc56850f96e85f6dd9cd6f4cc265a6ce69d
jonghwan-code <yang841025@naver.com>
2023-10-06 11:00:11 +0900

    updated week4 tests

--- 이상한문자만들기/answer.py
+++ 이상한문자만들기/answer.py
@@ -3,9 +3,6 @@ def solution(s):
     for i in range(len(arr)):
         tmp = list(arr[i])
         for j in range(len(tmp)):
-            if j % 2:
-                tmp[j] = tmp[j].lower()
-            else:
-                tmp[j] = tmp[j].upper()
+            tmp[j] = tmp[j].lower() if j % 2 else tmp[j].upper()
         arr[i] = ''.join(tmp)
     return ' '.join(arr)
