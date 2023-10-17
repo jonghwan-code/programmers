00a6b3e703044555eceb6427521e3217d413b6be
jonghwan-code <yang841025@naver.com>
2023-10-18 01:06:08 +0900

    1018 commit

--- 모의고사/answer.py
+++ 모의고사/answer.py
@@ -1,16 +1,13 @@
 def test(tester, answers):
     t, k = divmod(len(answers), len(tester))
-    return [True if a == b else False for a, b in zip(tester*t + tester[:k], answers)]
-    
+    return [a == b for a, b in zip(tester*t + tester[:k], answers)]
+
+
 def solution(answers):
     oneP = test([1, 2, 3, 4, 5], answers).count(True)
     twoP = test([2, 1, 2, 3, 2, 4, 2, 5], answers).count(True)
     thiP = test([3, 3, 1, 1, 2, 2, 4, 4, 5, 5], answers).count(True)
-    tmp = [oneP, twoP, thiP]
-    mx = max(tmp)
-    res = []
-    for idx, val in enumerate(tmp):
-        print(idx, val, mx)
-        if val == mx:
-            res.append(idx+1)
-    return res
+    student_grade = [(1, oneP), (2, twoP), (3, thiP)]
+    mx = max([p for s, p in student_grade])
+    filtered = [(a, b) for a, b in student_grade if b == mx]
+    return list(map(lambda x: x[0], filtered))
